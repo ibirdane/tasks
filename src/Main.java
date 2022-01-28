fce5c6944bae13d8a7ac1d0d173cbc9052fbcff3
ibirdane <ibirdane16@gmail.com>
2022-01-28 12:28:15 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+
+        for (int i = num - 1; i >= 1; i--) {
+            num = num * i;
+        }
+        System.out.println(num);
+
+    }
+
+
+
+}
