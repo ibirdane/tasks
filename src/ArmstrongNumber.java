ff03f5b4ea79c035119ea93fe5f9c0d95d47e9c2
ibirdane <ibirdane16@gmail.com>
2022-01-28 15:20:24 +0100

    genelToplam

--- src/ArmstrongNumber.java
+++ src/ArmstrongNumber.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class ArmstrongNumber {
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+
+    public static void main(String[] args) {
+
+        System.out.println("Sayini giriniz");
+        Scanner sc = new Scanner(System.in);
+        long num = sc.nextLong();
+        int basamak=1;
+        long sayi = num;
+        while(sayi>10){
+            sayi = sayi/10;
+            ++basamak;
+        }
+        long toplam=0;
+       long bolen=1;
+        while(bolen<num/10){
+            bolen*=10;
+        }
+        System.out.println(bolen);
+        long genelToplam=0;
+        while(bolen>0){
+
+            toplam=toplam+num/bolen;
+            num=num-((num/bolen)*bolen);
+            bolen=bolen/10;
+        }
+
+        System.out.println(toplam);
+
+    }
 }
