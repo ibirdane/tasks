a6f768c2e4a36cccdd9778f673ed487ddc2eebbc
ibirdane <ibirdane16@gmail.com>
2022-01-31 13:33:51 +0100

    fibonacci

---
+++ src/fibonacci.java
@@ -0,0 +1,16 @@
+public class fibonacci {
+    static int fib(int num){
+
+        if(num==1||num==2){
+            return 1;
+        }
+        return fib(num-1)+fib(num-2);
+    }
+    public static void main(String[] args) {
+
+        System.out.println(fib(9));
+
+    }
+
+
+}
