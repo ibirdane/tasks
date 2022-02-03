b0b8fc2ab13fbcaec7fac530c1394e222ec8b3d6
ibirdane <ibirdane16@gmail.com>
2022-02-03 11:24:40 +0100

    arrayToplam

---
+++ src/Arrays/AvgArr.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class AvgArr {
+    public static void main(String[] args) {
+
+        int [] arr = {1,2,3,4,5};
+        System.out.println(arrAvg(arr));
+
+
+    }
+
+   public static double arrAvg(int[] arr){
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        double avg = sum/arr.length;
+        return avg;
+    }
+}
